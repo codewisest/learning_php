c80bab73119751031fda413613a218f221fc4f99
Chijioke <wisestcodes@gmail.com>
2023-05-15 14:15:54 -0400

    variable manipulation

--- index.php
+++ index.php
@@ -8,4 +8,12 @@ echo $age;
 
 $doubleAge = $age * 2;
 echo $doubleAge;
+
+define('VERSION', 1.2);
+echo VERSION;
+
+$firstName = 'Chiji';
+echo 'My name is ' . $firstName . ' the great ';
+
+echo "My name is $firstName the great";
 ?>
