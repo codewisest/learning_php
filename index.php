3d8232fd952464b0e4b37016ef1084199ed84745
Chijioke <wisestcodes@gmail.com>
2023-05-27 21:34:34 -0400

    associative arrays

--- index.php
+++ index.php
@@ -15,5 +15,38 @@ echo VERSION;
 $firstName = 'Chiji';
 echo 'My name is ' . $firstName . ' the great ';
 
-echo "My name is $firstName the great";
+echo '<p>' . "My name is $firstName the great" . '</p>';
+
+// arrays
+// indexable arrays
+$colors = array('Red', 'Green', 'Blue');
+
+print_r($colors);
+echo '<p>' . $colors[1] . '</p>';
+
+// add item to array
+$colors[] = 'yellow';
+print_r($colors);
+
+// associative array
+$home_towns = array(
+    'Chiji' => 'Umunze',
+    'Gloria' => 'Umunne',
+    'Uzor' => 'Umuaka',
+    'Marvelous' => 'Umuenyi',
+);
+
+echo '<pre>';
+print_r($home_towns);
+echo '</pre>';
+
+$name = 'Chiji';
+$city = 'Bowmanville';
+$movie = 'God is not dead';
+$friend = 'Gloria';
+$fruit = 'Bananas';
+
+echo "<h1> Hello $name </h1>";
+echo "<p>We will be going to $city with $friend to see $movie. We will be going with a bunch of $fruit</p>";
+
 ?>
