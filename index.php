dce14e63a2721940d81c251cf6708de76d13ecbd
Chijioke <wisestcodes@gmail.com>
2023-05-29 21:46:01 -0400

    match statement

--- index.php
+++ index.php
@@ -107,5 +107,14 @@ switch($turtle) {
         echo '<p>No bandana colour</p>';
 }
 
-echo "<p>$bandana</p>"
+echo "<p>$bandana</p>";
+
+$x = 0;
+$result = match($x) {
+    1 => '$x is 1',
+    2 => '$x is 2',
+    default => '$x is neither 1 nor 2',
+};
+echo $result;
+phpinfo();
 ?>
