8ff379205aacc1722e27bdb014f6b193770f9f83
Chijioke <wisestcodes@gmail.com>
2023-07-06 23:58:37 -0400

    form submission

--- add.php
+++ add.php
@@ -1,27 +1,46 @@
 <?php
+$title = $email = $ingredients = '';
+    $errors = array('email' => '', 'title' => '', 'ingredients' => '');
     if(isset($_POST['submit'])){
         // echo htmlspecialchars($_POST['email']);
         // echo htmlspecialchars($_POST['title']);
         // echo htmlspecialchars($_POST['ingredients']);
         // check email
         if(empty($_POST['email'])) {
-            echo 'An email is required <br />';
+            $errors['email'] = 'An email is required <br />';
         } else {
-            echo htmlspecialchars($_POST['email']);
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'Email must be a valid email sddress';
+            }
+            
         }
 
         // check title
         if(empty($_POST['title'])) {
-            echo 'A title is required <br />';
+            $errors['title'] = 'A title is required <br />';
         } else {
-            echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                $errors['title'] = 'Title must be letters only';
+            }
         }
 
         // check ingredients
         if(empty($_POST['ingredients'])){
-            echo 'An ingredient is required';
+            $errors['ingredients'] = 'An ingredient is required';
         } else {
-            echo htmlspecialchars($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                $errors['ingredients'] = 'Ingredients must me letters and spaces only';
+            }
+            
+        }
+        if(array_filter($errors)) {
+            // echo 'There are errors in the form';
+        } else {
+            // echo 'form is valid';
+            header('Location: index.php');
         }
     }
 ?>
@@ -35,11 +54,14 @@
     <h4>Add a Pizza</h4>
     <form action="add.php" method="POST">
         <label for="email">Your Email:</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email" value="<?php echo $email; ?>">
+        <div><?php echo $errors['email']; ?></div>
         <label for="email">Pizza Title:</label>
-        <input type="text" name="title" id="title">
+        <input type="text" name="title" id="title" value="<?php echo $title; ?>">
+        <div><?php echo $errors['title']; ?></div>
         <label for="email">Ingredients</label>
-        <input type="text" name="ingredients" id="ingredients">
+        <input type="text" name="ingredients" id="ingredients" value="<?php echo $ingredients; ?>">
+        <div><?php echo $errors['ingredients']; ?></div>
         <div>
             <input type="submit" value="submit" name="submit">
         </div>
