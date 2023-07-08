f45945cd8deb23790f8d09da7184bf8a7ab55f73
Chijioke <wisestcodes@gmail.com>
2023-07-07 20:30:23 -0400

    explode ingredients

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ $success = mysqli_real_connect(
     $query = 'SELECT title, ingredients, id FROM pizzas';
     $result = mysqli_query($link, $query);
     mysqli_close($link);
+    // $row = mysqli_fetch_assoc($result);
+    // print_r(explode(',', $row['ingredients']));
   
 ?>
 <!DOCTYPE html>
@@ -31,7 +33,7 @@ $success = mysqli_real_connect(
     include('templates/header.php');
 ?>
  <h4>Pizzas</h4>
- <?php print_r($row) ?>
+ <!-- <?php print_r($row) ?> -->
  <div>
     <div>
         <?php while($row = mysqli_fetch_assoc($result)){ ?>
@@ -39,7 +41,11 @@ $success = mysqli_real_connect(
                 <div>
                     <div>
                         <h6><?php echo htmlspecialchars($row['title']); ?></h6>
-                        <div><?php echo htmlspecialchars($row['ingredients']); ?></div>
+                        <ul>
+                            <?php foreach(explode(',', $row['ingredients']) as $ingredient) {?>
+                                    <li><?php echo htmlspecialchars($ingredient)?></li>
+                            <?php } ?>
+                        </ul>
                     </div>
                     <div>
                         <a href="#">More info</a>
