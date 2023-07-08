b5903d8e226f9f3b760f0653df4f6266395c41f1
Chijioke <wisestcodes@gmail.com>
2023-07-07 20:11:24 -0400

    retrieve data from db

--- index.php
+++ index.php
@@ -1,9 +1,54 @@
+<?php
+    $user = 'wisestme';
+$password = 'dummy1234';
+$db = 'pizza_domain';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+// check connection
+    if(!$success) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    $query = 'SELECT title, ingredients, id FROM pizzas';
+    $result = mysqli_query($link, $query);
+    mysqli_close($link);
+  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php
     include('templates/header.php');
 ?>
- 
+ <h4>Pizzas</h4>
+ <?php print_r($row) ?>
+ <div>
+    <div>
+        <?php while($row = mysqli_fetch_assoc($result)){ ?>
+            <div>
+                <div>
+                    <div>
+                        <h6><?php echo htmlspecialchars($row['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($row['ingredients']); ?></div>
+                    </div>
+                    <div>
+                        <a href="#">More info</a>
+                    </div>
+                </div>
+            </div>
+        <?php }?>
+    </div>
+ </div>
  <?php
     include('templates/footer.php');
 ?>
