18550f59b2f778a4369aa473f7aaad5592d902a5
Chijioke <wisestcodes@gmail.com>
2023-07-10 17:27:13 -0400

    set cookie

--- playground.php
+++ playground.php
@@ -280,4 +280,40 @@ function formatProduct($product) {
 
 $formatted = formatProduct(['name' => 'gold star', 'price' => 500]);
 echo $formatted;
+
+echo $_SERVER['SERVER_NAME'] . '<br />';
+echo $_SERVER['REQUEST_METHOD'] . '<br />';
+echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+echo $_SERVER['PHP_SELF'] . '<br />';
+
+if(isset($_POST['submit'])){
+    // cookie for gender
+    setcookie('gender', $_POST['gender'], time() + 86400);
+    session_start();
+
+    $_SESSION['name'] = $_POST['name'];
+    echo $_SESSION['name'];
+
+    header('Location: index.php');
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        <input type="text" name="name">
+        <select name="gender" id="gender">
+            <option value="male">male</option>
+            <option value="female">female</option>
+        </select>
+        <input type="submit" value="Submit" name="submit">
+    </form>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,14 @@
+<?php
+    session_start();
+    if($_SERVER['QUERY_STRING'] == 'noname') {
+        // unset($_SESSION['name'])
+        session_unset();
+    } 
+    $name = $_SESSION['name'] ?? 'Guest';
 
+    // get cookie
+    $gender = $_COOKIE['gender'] ?? 'unknown';
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +20,9 @@
         <div class="container">
             <a href="index.php">Pizza Domain</a>
             <ul>
+                <li>Hello <?php echo htmlspecialchars($name) ?></li>
+                <li>(<?php echo htmlspecialchars($gender) ?>)</li>
+
                 <li><a href="add.php">Add a Pizza</a></li>
             </ul>
         </div>
